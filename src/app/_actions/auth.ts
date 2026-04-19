91e6355d43252dc7e72bc06564e9f28fb290635c
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2026-04-19 22:21:02 +0200

    feat: implement authentication actions for user login and sign out

---
+++ src/app/_actions/auth.ts
@@ -0,0 +1,53 @@
+"use server"
+
+import { createAdminClient, createSessionClient } from "@/lib/appwrite"
+import { actionClient } from "@/lib/next-safe-actions"
+import { catchError } from "@/lib/utils"
+import { AppwriteException } from "appwrite"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import { loginUserResponseSchema, loginUserSchema } from "../(home)/_schemas"
+
+export const loginUser = actionClient
+  .inputSchema(loginUserSchema)
+  .outputSchema(loginUserResponseSchema)
+  .action(async ({ parsedInput: { email, password } }) => {
+    const { account } = await createAdminClient()
+    const cookiesValues = await cookies()
+
+    const [error, data] = await catchError(
+      account.createEmailPasswordSession({
+        email,
+        password,
+      })
+    )
+
+    if (data) {
+      cookiesValues.set("my-custom-session", data.secret, {
+        path: "/",
+        httpOnly: true,
+        sameSite: "strict",
+        secure: true,
+      })
+    }
+
+    if (error) {
+      if (error instanceof AppwriteException) {
+        throw new Error(error.message ?? "An unexpected server error occurred")
+      }
+
+      throw new Error(error.message ?? "An unexpected error occurred")
+    }
+
+    return { id: data.$id, success: true }
+  })
+
+export const signOut = actionClient.action(async () => {
+  const { account } = await createSessionClient()
+  const cookiesValues = await cookies()
+
+  cookiesValues.delete("my-custom-session")
+  await account.deleteSession({ sessionId: "current" })
+
+  redirect("/")
+})
