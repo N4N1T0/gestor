f15601efa07e91bb436a7d7b37773e5e660eb8bc
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2026-04-19 22:07:21 +0200

    feat: add types for Clients, NavMainItem, and BreadcrumbItemData

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface NavMainItem {
+  title: string
+  url: string
+  icon: React.ReactNode
+  isActive: boolean
+}
+
+export interface BreadcrumbItemData {
+  label: string
+  href: string
+}
