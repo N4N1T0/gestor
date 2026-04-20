5a3a2dbc77db88dcfc1c6354a805fdad2ab7dd1d
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2026-04-20 18:40:07 +0200

    fix: update import path for Models and adjust NavMainItem type definition

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export interface NavMainItem {
-  title: string
+  title: NavMainItems
   url: string
   icon: React.ReactNode
   isActive: boolean
@@ -8,4 +8,24 @@ export interface NavMainItem {
 export interface BreadcrumbItemData {
   label: string
   href: string
+}
+
+export interface SecondSidebarCardData {
+  id: string
+  slug: string
+  title: string
+  meta: string
+  description: string
+}
+
+export enum NavMainItems {
+  INVOICES = "Facturas",
+  EXPENSES = "Gastos",
+  CLIENTS = "Clientes",
+  SUPPLIERS = "Proveedores",
+}
+
+// COMMON
+export interface PageParams {
+  params: Promise<{ [key: string]: string | string[] | undefined }>
 }
